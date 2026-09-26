a02f24adeda5e8d667e968b36c18281e9a006f79
Sanjana N Bharadwaj <sanjananbharadwaj3@gmail.com>
2026-09-26 12:53:02 +0530

    Add Neon database support and remove node_modules

--- parampara-api/db.js
+++ parampara-api/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-});
+const poolConfig = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  : {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD
+    };
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
