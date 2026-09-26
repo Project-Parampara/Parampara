07bc30ad1e3e1a713634d9fbb95baa62506708cd
Sanjana N Bharadwaj <sanjananbharadwaj3@gmail.com>
2026-09-26 13:55:58 +0530

    Fix Render server port binding

--- parampara-api/index.js
+++ parampara-api/index.js
@@ -470,6 +470,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+// Start server
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on 0.0.0.0:${PORT}`);
 });
